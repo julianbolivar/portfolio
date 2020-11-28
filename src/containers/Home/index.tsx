9ef73a5af2419ac52009a5bb52a259a57c469ed2
Boken SAS <bokensas@Bokens-iMac.local>
2020-11-28 12:02:26 -0500

    Scroll interaction in progress

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import { Title } from "components/ui/Text";
+import styles from "./styles.module.css";
 
 const Home: React.FC = () => {
-  return <Title>Inicio</Title>;
+  return (
+    <section className={styles.home}>
+      <Title>Home</Title>
+    </section>
+  );
 };
 
 export default Home;
